bc5c445f3ec07ec1e73e6a89cc074efc8e55686d
dongxulee <leodongxu@gmail.com>
2021-12-20 10:13:29 -0500

    change the gamma from 3 to 4 to approach targeting saving and investment ratio

---
+++ 20211220/simulation_betas.py
@@ -0,0 +1,29 @@
+from simulation import *
+import os.path
+from multiprocessing import Pool 
+
+AgentType = ["poorHigh", "poorLow", "richHigh", "richLow"]
+Beta_r = [0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.10]
+Gamma = [3.0, 4.0]
+
+def mapOverBeta(beta_r):
+    for agentType in AgentType:
+        for gamma in Gamma:
+            fileName = agentType + "_" + str(beta_r) + "_" + str(gamma)
+            if os.path.exists("waseozcbkhm_" + fileName + ".npy"):
+                break 
+            if not os.path.exists(fileName + ".npy"):
+                break
+            '''
+                Constants 
+            '''
+            # discounting factor
+            beta = 1/(1+beta_r)
+            # utility function parameter 
+            print("agentType: ", agentType)
+            print("beta: ", beta)
+            print("gamma: ", gamma)
+            simulation(beta_r, agentType, gamma, fileName)
+
+p = Pool(10)
+p.map(mapOverBeta, Beta_r)
