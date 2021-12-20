fe643268244bc80554903bf5c56bca8d24f82e52
dongxulee <leodongxu@gmail.com>
2021-12-20 08:05:34 -0500

    solve for gamma 3

--- 20211218/simulation_betas.py
+++ 20211218/simulation_betas.py
@@ -3,8 +3,8 @@ import os.path
 from multiprocessing import Pool 
 
 AgentType = ["poorHigh", "poorLow", "richHigh", "richLow"]
-Beta_r = [0.05]
-Gamma = [3.0]
+Beta_r = [0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.10]
+Gamma = [3.0, 4.0]
 
 def mapOverBeta(beta_r):
     for agentType in AgentType:
@@ -25,5 +25,5 @@ def mapOverBeta(beta_r):
             print("gamma: ", gamma)
             simulation(beta_r, agentType, gamma, fileName)
 
-p = Pool(1)
+p = Pool(10)
 p.map(mapOverBeta, Beta_r)
