6ca9670dea50150aabe31f86578323cec0ab018c
dongxulee <leodongxu@gmail.com>
2022-01-20 14:34:10 -0500

    change the age of buying and check the result variation

---
+++ 20220121/solveMDP_agentType.py
@@ -0,0 +1,21 @@
+from solveMDP import *
+from multiprocessing import Pool 
+
+AgentType = ["poorHigh", "poorLow", "richHigh", "richLow"]
+Beta_r = [0.01,0.03,0.05,0.07,0.10]
+Gamma = [3.0, 4.0]
+
+
+def mapOverType(agentType):
+    for beta_r in Beta_r:
+         for gamma in Gamma:
+            # discounting factor
+            beta = 1/(1+beta_r)
+            # utility function parameters 
+            print("agentType: ", agentType)
+            print("beta: ", beta)
+            print("gamma: ", gamma)
+            solveMDP(beta_r, agentType, gamma)
+
+p = Pool(4)
+p.map(mapOverType, AgentType)
---
+++ 20220122/simulation_betas.py
@@ -0,0 +1,29 @@
+from simulation import *
+import os.path
+from multiprocessing import Pool 
+
+AgentType = ["poorHigh", "poorLow", "richHigh", "richLow"]
+Beta_r = [0.01,0.03,0.05,0.07,0.10]
+Gamma = [3.0, 4.0]
+
+def mapOverBeta(beta_r):
+    for gamma in Gamma:
+        for agentType in AgentType:
+            fileName = agentType + "_" + str(beta_r) + "_" + str(gamma)
+            if os.path.exists("waseozcbkhm_" + fileName + ".npy"):
+                break 
+            if not os.path.exists(fileName + ".npy"):
+                break
+            '''
+                Constants 
+            '''
+            # discounting factor
+            beta = 1/(1+beta_r)
+            # utility function parameter 
+            print("agentType: ", agentType)
+            print("beta: ", beta)
+            print("gamma: ", gamma)
+            simulation(beta_r, agentType, gamma, fileName)
+
+p = Pool(5)
+p.map(mapOverBeta, Beta_r)
