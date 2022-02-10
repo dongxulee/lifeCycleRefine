3f5af78001349699af04d2b72f6be28cc01c5625
dongxulee <leodongxu@gmail.com>
2022-02-10 17:13:50 -0500

    different aggregate shocks

---
+++ 20220213/simulationParallel_betas.py
@@ -0,0 +1,30 @@
+from simulationParallel import *
+import os.path
+from multiprocessing import Pool 
+
+AgentType = ["poorHigh", "poorLow", "richHigh", "richLow"]
+Beta_r = [0.01,0.05,0.10]
+Gamma = [4.0]
+
+def mapOverBeta(beta_r):
+    for gamma in Gamma:
+        for agentType in AgentType:
+            print(agentType)
+            fileName = agentType + "_" + str(beta_r) + "_" + str(gamma)
+            if os.path.exists("parallel_waseozcbkhm_" + fileName + ".npy"):
+                break 
+            if not os.path.exists(fileName + ".npy"):
+                break
+            '''
+                Constants 
+            '''
+            # discounting factor
+            beta = 1/(1+beta_r)
+            # utility function parameter 
+            print("agentType: ", agentType)
+            print("beta: ", beta)
+            print("gamma: ", gamma)
+            simulation(beta_r, agentType, gamma, fileName)
+
+p = Pool(3)
+p.map(mapOverBeta, Beta_r)
