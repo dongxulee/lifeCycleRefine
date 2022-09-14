07ba04b0a6111f828b7d5d3d1fd094c473de1941
dongxulee <leodongxu@gmail.com>
2022-09-14 10:26:51 -0400

    adding cluster experiments

--- 20220902/solveMDP_agentType.py
+++ 20220902/solveMDP_agentType.py
@@ -1,9 +1,9 @@
 from solveMDP import *
-from multiprocessing import Pool 
+from multiprocessing import Pool
 
 AgentType = ["poorHigh", "poorLow", "richHigh", "richLow"]
-Beta_r = [0.02,0.04,0.06,0.08,0.10]
-Gamma = [4.0, 4.5, 5.0, 5.5, 6.0]
+Beta_r = [0.02, 0.04, 0.06, 0.08, 0.10]
+Gamma = [4.0, 5.0, 6.0]
 
 
 def mapOverType(agentType):
